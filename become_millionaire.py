db1c9fc0d8d61dbb659332c081c2f9add01719f5
kodywynne <65366132+kodywynne@users.noreply.github.com>
2022-05-17 19:58:27 -0700

    Update become_millionaire.py
    Fixed equations. Wrong Variable was in the Equation. WORKS! Need to add more features.

--- become_millionaire.py
+++ become_millionaire.py
@@ -11,18 +11,15 @@ monthly_expenses = input("Please enter your total monthly expenses: \n")
 monthly_play_money = input("Enter any additional expenses for leisure activities: \n")
 current_savings = input("Please enter the dollar figure \nof your current savings: \n")
 
-
-
 monthly_excess = int(income) / 12 - int(monthly_expenses) - int(monthly_play_money)
-
-total_current_savings = int(current_savings) + int(monthly_excess)
-
+total_current_savings = int(current_savings) + monthly_excess
 counter = 1
 
 final_savings = total_current_savings
-while counter < 1000000:
+print(final_savings)
+while counter < 10000:
     counter = counter + 1
-    final_savings = final_savings + total_current_savings
+    final_savings = final_savings + monthly_excess
     if final_savings >= 1000000:
         years = counter / 12
         print("It will take " + str(counter) + " months, or " + str(years) + " years to become a millionaire")
